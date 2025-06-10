6fe72f392a47005d0a6989f4d4ca9ca2bc3d827c
Keyur Amin <keyuramin08@gmail.com>
2025-06-10 16:48:29 +1000

    93

--- assets/js/general.js
+++ assets/js/general.js
@@ -32,10 +32,7 @@ jQuery( document ).ready(function($) {
              return [date.getDay() != 0, ''];
           }; 
     });
-    //  $('#shipping_date').datepicker({
-    //     dateFormat: 'dd-mm-yy',
-    //      minDate: '+15d', // Set the minimum date to today
-    // });
+
      $("body").on("change", "#shipping_date", function(){
         
         var pickupdate = $(this).val();
@@ -55,7 +52,7 @@ jQuery( document ).ready(function($) {
         });
     }); 
    
-     var $variationForm = $('#variations_form');
+    var $variationForm = $('#variations_form');
 
     $('.variations_form select').on('change', function() {
         var productVariations = JSON.parse($(".variations_form").attr("data-product_variations"));
@@ -69,4 +66,51 @@ jQuery( document ).ready(function($) {
     $(document).on('change', '.nbt-location-selector', function() {
         $('#nbt-location-selector-form').submit();
     });
+
+    // Location-based price update for variable products
+    if ($('.variations_form').length) {
+        var currentLocation = '';
+        if (typeof Cookies !== 'undefined') {
+            currentLocation = Cookies.get('location_price') || '';
+        } else {
+            // fallback: try to get from select
+            currentLocation = $('.nbt-location-selector').val() || '';
+        }
+        function updateVariationPriceDisplay(variation) {
+            if (!variation) return;
+            var priceHtml = '';
+            if (currentLocation && variation['_' + currentLocation + '_price']) {
+                var price = variation['_' + currentLocation + '_price'];
+                var salePrice = variation['_' + currentLocation + '_sale_price'];
+                if (salePrice && salePrice !== '' && salePrice !== '0') {
+                    priceHtml = '<del>' + variation.display_price + '</del> <ins>' + salePrice + '</ins>';
+                } else {
+                    priceHtml = price;
+                }
+            } else {
+                // fallback to default WooCommerce price_html
+                priceHtml = variation.price_html;
+            }
+            // Update the price display
+            $('.single_variation .woocommerce-Price-amount, .single_variation .price, .product .price').html(priceHtml);
+        }
+        $('.variations_form').on('show_variation', function(event, variation) {
+            updateVariationPriceDisplay(variation);
+        });
+        // Also update on location change
+        $(document).on('change', '.nbt-location-selector', function() {
+            var selectedVariation = $('.variations_form').data('variation_id');
+            var productVariations = JSON.parse($('.variations_form').attr('data-product_variations'));
+            var found = null;
+            for (var i = 0; i < productVariations.length; i++) {
+                if (productVariations[i].variation_id == selectedVariation) {
+                    found = productVariations[i];
+                    break;
+                }
+            }
+            if (found) {
+                updateVariationPriceDisplay(found);
+            }
+        });
+    }
 });
