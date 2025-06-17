6d5c52df02593b16ea9802fa62dbe17c08b5955a
Keyur Amin <keyuramin08@gmail.com>
2025-06-17 14:02:49 +1000

    4

--- assets/js/general.js
+++ assets/js/general.js
@@ -66,4 +66,40 @@ jQuery( document ).ready(function($) {
     $(document).on('change', '.nbt-location-selector', function() {
         $('#nbt-location-selector-form').submit();
     });
+
+    // --- Addon enable/disable logic for variable products ---
+    function setAddonsEnabled(enabled) {
+        // Target all addon input fields inside .yith-wapo-addon
+        var $addonFields = $('.yith-wapo-addon :input, .yith-wapo-addon select, .yith-wapo-addon textarea');
+        $addonFields.prop('disabled', !enabled);
+        // Show/hide overlay message
+        if (!enabled) {
+            if ($('#nbt-addon-overlay').length === 0) {
+                var overlay = $('<div id="nbt-addon-overlay" style="position:absolute;top:0;left:0;width:100%;height:100%;background:rgba(255,255,255,0.7);z-index:10;display:flex;align-items:center;justify-content:center;"><span style="font-size:1.1em;color:#444;background:#fff;padding:12px 24px;border-radius:8px;box-shadow:0 2px 8px rgba(0,0,0,0.08);">Please select a variation to enable addons.</span></div>');
+                // Position overlay over the addons container
+                var $container = $('#yith-wapo-container');
+                $container.css('position', 'relative');
+                $container.append(overlay);
+            }
+        } else {
+            $('#nbt-addon-overlay').remove();
+        }
+    }
+
+    function isVariationSelected() {
+        // WooCommerce sets a hidden input with name="variation_id" when a variation is selected
+        var $form = $('.variations_form');
+        var variationId = $form.find('input[name="variation_id"]').val();
+        return variationId && variationId !== '' && variationId !== '0';
+    }
+
+    // Only apply for variable products with addons
+    if ($('.variations_form').length && $('#yith-wapo-container').length) {
+        // On page load
+        setAddonsEnabled(isVariationSelected());
+        // On variation selection
+        $('.variations_form').on('found_variation hide_variation reset_data', function(e) {
+            setAddonsEnabled(isVariationSelected());
+        });
+    }
 });
